570f7d6cd4385c8994b11f972520a81898981f25
Juan Villada <juancamilovillada@gmail.com>
2018-10-30 16:26:35 +0800

    added ignore

--- src/random_net_process.R
+++ src/random_net_process.R
@@ -1,6 +1,52 @@
-#install.packages("igraph")
 library("igraph")
 
+random_net <- sample_gnp(n = 50, p=0.1, directed = FALSE, loops = FALSE)
+plot(random_net)
+degree(random_net)
+degree_distribution(random_net)
+
+mean_distance(graph = random_net, directed = FALSE)
+average.path.length(random_net, directed=FALSE, unconnected=TRUE)
+
+
+#mean_distance calculates the average path length in a graph, by calculating the shortest paths between all pairs of vertices (both ways for directed graphs). This function does not consider edge weights currently and uses a breadth-first search.
+mean_distance(graph = random_net, directed = FALSE)
+
+mean(c(distances(random_net, mode="out")))
+mean(c(shortest.paths(random_net)))
+
+#### ANSWER! (https://stackoverflow.com/questions/42352578/mean-distance-vs-mean-of-disances-in-igraph)
+random_net_lengths <- distances(random_net)[lower.tri(distances(random_net))]
+mean(random_net_lengths)
+random_net_lengths[!is.finite(random_net_lengths)] <- NA
+mean(random_net_lengths, na.rm=TRUE)
+boxplot(random_net_lengths)
+
+# "gnm" for nodes and edeges or "gnp" for nodes and probability of egding
+random_net1 <- erdos.renyi.game(n=50, p.or.m = 60, type=c("gnm"), directed = FALSE, loops = FALSE)
+random_net1_lengths <- distances(random_net1)[lower.tri(distances(random_net1))]
+plot(random_net1)
+mean(random_net1_lengths)
+random_net1_lengths[!is.finite(random_net1_lengths)] <- NA
+mean(random_net1_lengths, na.rm=TRUE)
+
+random_net2 <- erdos.renyi.game(n=50, p.or.m = 60, type=c("gnm"), directed = FALSE, loops = FALSE)
+random_net2_lengths <- distances(random_net2)[lower.tri(distances(random_net2))]
+plot(random_net2)
+mean(random_net2_lengths)
+random_net2_lengths[!is.finite(random_net2_lengths)] <- NA
+mean(random_net2_lengths, na.rm=TRUE)
+
+boxplot(x = list(random_net1 = random_net1_lengths,
+                 random_net2 = random_net2_lengths))
+
+length(random_net1_lengths)
+length(random_net2_lengths)
+wilcox.test(random_net1_lengths, random_net2_lengths)
+
+
+
+#---- LOOP ----#
 oberved_net <- erdos.renyi.game(n=50, p.or.m = 60, type=c("gnm"), directed = FALSE, loops = FALSE)
 oberved_net_lengths <- distances(oberved_net)[lower.tri(distances(oberved_net))]
 plot(oberved_net)
